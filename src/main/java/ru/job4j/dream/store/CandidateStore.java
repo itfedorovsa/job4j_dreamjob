a38f98cbb2688e4baf55c8e90e16e77e45e358f0
FedorovSA <itfedorovsa@gmail.com>
2022-09-07 22:44:38 +0300

    List of candidates

---
+++ src/main/java/ru/job4j/dream/store/CandidateStore.java
@@ -0,0 +1,28 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Candidate;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CandidateStore {
+    private static final CandidateStore CANDIDATE_INST = new CandidateStore();
+
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    public CandidateStore() {
+        candidates.put(1, new Candidate(1, "John", "desc1", LocalDateTime.now()));
+        candidates.put(2, new Candidate(2, "Jade", "desc2", LocalDateTime.now()));
+        candidates.put(3, new Candidate(3, "Jim", "desc3", LocalDateTime.now()));
+    }
+
+    public static CandidateStore instanceOf() {
+        return CANDIDATE_INST;
+    }
+
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}

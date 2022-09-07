d5f5da0335d082c99d4062363a3b6ba39ffa3882
FedorovSA <itfedorovsa@gmail.com>
2022-09-07 21:40:34 +0300

    Thymeleaf basics

---
+++ src/main/java/ru/job4j/dream/store/PostStore.java
@@ -0,0 +1,28 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Post;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PostStore {
+    private static final PostStore INST = new PostStore();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private PostStore() {
+        posts.put(1, new Post(1, "Junior Java Job", "post1", LocalDateTime.now()));
+        posts.put(2, new Post(2, "Middle Java Job", "post2", LocalDateTime.now()));
+        posts.put(3, new Post(3, "Senior Java Job", "post3", LocalDateTime.now()));
+    }
+
+    public static PostStore instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}

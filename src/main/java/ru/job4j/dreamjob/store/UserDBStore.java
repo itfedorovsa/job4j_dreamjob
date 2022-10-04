8e6319907aac58e2e567d573818d1b89ae9a69f0
FedorovSA <itfedorovsa@gmail.com>
2022-10-04 12:54:32 +0300

    Multithreading in DB (update)

--- src/main/java/ru/job4j/dreamjob/store/UserDBStore.java
+++ src/main/java/ru/job4j/dreamjob/store/UserDBStore.java
@@ -2,7 +2,6 @@ package ru.job4j.dreamjob.store;
 
 import net.jcip.annotations.ThreadSafe;
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.postgresql.util.PSQLException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Repository;

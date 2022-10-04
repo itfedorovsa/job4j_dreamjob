d943eb6b4d3f1028ccff0924f3a8528a252184ae
FedorovSA <itfedorovsa@gmail.com>
2022-10-04 16:08:36 +0300

    HttpSession

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -1,15 +1,25 @@
 package ru.job4j.dreamjob.controller;
 
 import net.jcip.annotations.ThreadSafe;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import ru.job4j.dreamjob.model.User;
+
+import javax.servlet.http.HttpSession;
 
 @ThreadSafe
 @Controller
 public class IndexController {
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            user = new User();
+            user.setName("Guest");
+        }
+        model.addAttribute("user", user);
         return "index";
     }
 }
